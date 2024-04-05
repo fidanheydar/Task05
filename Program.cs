0df2e1fe8c82152fb84696e8bd8058d8adcae6a5
Fidan Heydarova <125346195+fidanheydar@users.noreply.github.com>
2024-04-05 17:18:07 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿
+
+using ConsoleWeb;
+using ConsoleWeb.Entities;
+AppDbContext appDbContext = new AppDbContext();
+
+var appBuilder = WebApplication.CreateBuilder();
+
+appBuilder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", builder =>
+    {
+        builder.AllowAnyOrigin()
+               .AllowAnyHeader()
+               .AllowAnyMethod();
+    });
+});
+
+var app = appBuilder.Build();
+
+app.UseCors("AllowAll");
+
+app.MapGet("/", () =>
+{
+    return "Hello, World!";
+});
+
+app.MapGet("/products", () =>
+{
+    return appDbContext.Products.ToList();
+});
+
+app.MapGet("/products/{id}", (int id) =>
+{
+    var data = appDbContext.Products.Find(id);
+
+    if (data == null) return Results.NotFound("Product not found!");
+
+    return Results.Ok(data);
+});
+
+app.MapPost("/products", (Product product) =>
+{
+    appDbContext.Add(product);
+    appDbContext.SaveChanges();
+
+    return Results.Ok();
+});
+
+app.Run();
