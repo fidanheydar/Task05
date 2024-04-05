f4337580f276ec6acbdd54b0b402da6beb4d57c0
Fidan Heydarova <125346195+fidanheydar@users.noreply.github.com>
2024-04-05 17:15:28 +0400

    Add files via upload

---
+++ ConsoleWeb/ConsoleWeb/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using ConsoleWeb.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleWeb
+{
+    internal class AppDbContext:DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\MSSqlLocalDb;Database=WebDb ;Trusted_Connection=True");
+        }
+    }
+}
